4a59894241ec496031ea12348978756def8e5f20
Tyler Braddock <tyler.braddock@yahoo.com>
2017-04-12 21:17:49 +0000

    Enabled Devise capability of usernames. Generated migration file to add username column to users database table. Ran rake db:migrate. In application_controller.rb, added configure_permitted_parameters method and before_action to specify strong parameters. In model user.rb, added attr_accessor for :login and overwrote Devise method find_for_database_authentication. Also, added validations for username. In config/initializers/devise.rb, included :login in authentication_keys and set scoped_views to true. Updated devise views. For sessions/new.html.erb, changed :email to :login. For registrations/new.html.erb and registrations/edit.html.erb, added input for :username. In layouts/application.html.erb, created user-based top navigation links.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,29 @@
 class User < ApplicationRecord
+  # Virtual attribute for authenticating by either username or email
+  # This is in addition to a real persisted field like 'username'
+  attr_accessor :login
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+  validate :validate_username
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
+
+  # Overwrite Devise find_for_database_authentication method
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
 end
